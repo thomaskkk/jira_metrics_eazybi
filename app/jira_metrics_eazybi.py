a76f38581ae9a65391d9ec5322d213501d98cc8c
Thomas Kuryura <thomas.adm@gmail.com>
2022-06-18 18:57:00 -0300

    Dictionary return from csv

--- app/jira_metrics_eazybi.py
+++ app/jira_metrics_eazybi.py
@@ -1,21 +1,43 @@
 #!/usr/bin/env python
 
-import requests
-import csv
-# from pprint import pprint
+import os
+import confuse
+import pandas as pd
+from pprint import pprint
+
+
+cfg = confuse.Configuration('JiraMetricsEazybi', __name__)
+
+
+def get_eazybi_report(report_url):
+    dictio = pd.read_csv(report_url, delimiter=',')
+    dictio.columns = ['Date', 'Key', 'IssueType', 'CycleTime']
+    return dictio
+
+
+def metrics():
+    report_url = str(cfg['Report_URL'])
+    simulations = cfg['Montecarlo']['Simulations'].get()
+    simulation_days = cfg['Montecarlo']['Simulation_days'].get()
+    kanban_data = get_eazybi_report(report_url)
+    print(kanban_data)
+    """
+    ct = calc_cycletime_percentile(kanban_data, 85)
+    tp = calc_throughput(kanban_data)
+    mc = simulate_montecarlo(
+        tp, sources='Throughput',
+        simul=simulations,
+        simul_days=simulation_days)
+    """
 
 
 def main():
-    api_url = "https://aod.eazybi.com/accounts/70597/export/report/1075725-api-export.csv?embed_token=iid25hgzq92p8km005mmfz62q3cqchs58tbeudl24wi4dd64nsrsdcnnxapi"
-    with requests.Session() as s:
-        download = s.get(api_url)
-
-        decoded_content = download.content.decode('utf-8')
-
-        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-        my_list = list(cr)
-        for row in my_list:
-            print(row)
+    if os.path.isfile('config.yml'):
+        cfg.set_file('config.yml')
+        mc = metrics()
+        # print(mc)
+    else:
+        raise Exception("You don't have any valid config files")
 
 
 if __name__ == "__main__":
