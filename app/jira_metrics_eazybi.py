2a0c9c630606a1a26e4519246e6c99275b0deaf0
Thomas Kuryura <thomas.adm@gmail.com>
2022-06-18 19:17:41 -0300

    Cycletime percentile with ceil

--- app/jira_metrics_eazybi.py
+++ app/jira_metrics_eazybi.py
@@ -3,6 +3,7 @@
 import os
 import confuse
 import pandas as pd
+import numpy as np
 from pprint import pprint
 
 
@@ -11,18 +12,35 @@ cfg = confuse.Configuration('JiraMetricsEazybi', __name__)
 
 def get_eazybi_report(report_url):
     dictio = pd.read_csv(report_url, delimiter=',')
-    dictio.columns = ['Date', 'Key', 'IssueType', 'CycleTime']
+    dictio.columns = ['date', 'key', 'issuetype', 'cycletime']
     return dictio
 
+def calc_cycletime_percentile(kanban_data, percentile=None):
+    """Calculate cycletime percentiles on cfg with all dict entries"""
+    if kanban_data.empty is False:
+        if percentile is not None:
+            issuetype = kanban_data.groupby('issuetype').cycletime.quantile(
+                percentile / 100)
+            issuetype['Total'] = kanban_data.cycletime.quantile(
+                percentile / 100)
+            issuetype = issuetype.apply(np.ceil)
+            return issuetype
+        else:
+            for cfg_percentile in cfg['Cycletime']['Percentiles'].get():
+                cycletime = kanban_data.groupby(
+                    'issuetype').cycletime.quantile(
+                    cfg_percentile / 100)
+                cycletime['Total'] = kanban_data.cycletime.quantile(
+                    cfg_percentile / 100)
 
 def metrics():
     report_url = str(cfg['Report_URL'])
     simulations = cfg['Montecarlo']['Simulations'].get()
     simulation_days = cfg['Montecarlo']['Simulation_days'].get()
     kanban_data = get_eazybi_report(report_url)
-    print(kanban_data)
-    """
     ct = calc_cycletime_percentile(kanban_data, 85)
+    print(ct)
+    """
     tp = calc_throughput(kanban_data)
     mc = simulate_montecarlo(
         tp, sources='Throughput',
