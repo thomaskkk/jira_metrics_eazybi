82ebc58203e97de3fc16071cb7cf6d5b2f369cb4
Thomas Kuryura <thomas.adm@gmail.com>
2022-06-20 10:53:38 -0300

    Get ready for GCP Cloud run

--- app/main.py
+++ app/main.py
@@ -172,4 +172,4 @@ api.add_resource(HelloWorld, '/')
 api.add_resource(Eazybi, '/eazybi')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
